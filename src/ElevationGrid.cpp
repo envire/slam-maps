abb5b40b2bc89a1bb75ca3951bf2f3e6a0c58105
Anna Born <anna.born@dfki.de>
2015-10-20 10:08:47 +0200

    add elevation grid

---
+++ src/ElevationGrid.cpp
@@ -0,0 +1,88 @@
+#include "ElevationGrid.hpp"
+
+namespace envire 
+{
+namespace maps 
+{
+
+const std::string ElevationGrid::ELEVATION = "elevation_max"; // this will reference the max band
+const std::string ElevationGrid::ELEVATION_MIN = "elevation_min";
+const std::string ElevationGrid::ELEVATION_MAX = "elevation_max";
+
+const double ElevationGrid::ELEVATION_MIN_DEFAULT = std::numeric_limits<double>::infinity();
+const double ElevationGrid::ELEVATION_MAX_DEFAULT = -std::numeric_limits<double>::infinity();
+
+ElevationGrid::ElevationGrid() 
+    : Map() 
+{}
+
+ElevationGrid::ElevationGrid(GridConfig config)
+    : Map(config) 
+{
+    // add all layers to the grid without memory allocation and default value initialization
+    addGrid<double>(ELEVATION_MAX, ELEVATION_MAX_DEFAULT);
+    addGrid<double>(ELEVATION_MIN, ELEVATION_MIN_DEFAULT); 
+}
+
+ElevationGrid::~ElevationGrid()
+{
+
+}
+
+Eigen::Vector3d ElevationGrid::getNormal(const Index& idx) const
+{
+    if (!inGrid(idx))
+        throw std::runtime_error("Provided index is out of grid.");
+    if (!inGrid(idx + Index(1,1)))
+        throw std::runtime_error("Provided index should be at the distance Index(1,1) from the grid border.");
+    if (idx < Index(1,1))
+        throw std::runtime_error("Provided index should be at the distance Index(1,1) from the grid border.");            
+
+    size_t m = idx.x, n = idx.y;
+
+    const Grid<double> &elev_grid = getGrid<double>(ELEVATION);
+
+    double slope_x = (elev_grid.at(m - 1, n) - elev_grid.at(m + 1, n)) / (getScaleX() * 2.0); 
+    double slope_y = (elev_grid.at(m, n - 1) - elev_grid.at(m, n + 1)) / (getScaleY() * 2.0);
+
+    return Eigen::Vector3d( slope_x, slope_y, 1.0 ).normalized();
+}
+
+Eigen::Vector3d ElevationGrid::getNormal(const Eigen::Vector2d& pos) const
+{
+    Index idx;
+    if (!toGrid(pos, idx))
+        throw std::runtime_error("Provided position is out of the grid.");
+
+    return getNormal(idx);
+}
+
+double ElevationGrid::getMeanElevation(const Eigen::Vector2d& pos) const
+{
+    Index idx;
+    if (!toGrid(pos, idx))
+        throw std::runtime_error("Provided position is out of the grid.");
+
+    Eigen::Vector2d pos_t;
+    fromGrid(idx, pos_t);
+
+    //X and Y in the cell
+    pos_t.x() -= pos.x();
+    pos_t.y() -= pos.y();
+
+    Eigen::Vector3d normal = getNormal( pos );
+    double slope_x = normal.x() / normal.z();
+    double slope_y = normal.y() / normal.z();
+
+    const Grid<double> &elev_grid = getGrid<double>(ELEVATION);
+
+    double height = 
+        elev_grid.at(idx) +
+        pos_t.x() * slope_x +
+        pos_t.y() * slope_y;
+
+    return height;
+}
+
+}
+}
---
+++ src/ElevationGrid.hpp
@@ -0,0 +1,47 @@
+#ifndef __ENVIRE_MAPS_ELEVATION_GRID_HPP__
+#define __ENVIRE_MAPS_ELEVATION_GRID_HPP__
+
+#include "Map.hpp"
+
+namespace envire 
+{
+	namespace maps 
+	{
+		class ElevationGrid : public Map
+		{
+		public:
+
+			static const std::string ELEVATION;
+                  static const std::string ELEVATION_MIN;
+                  static const std::string ELEVATION_MAX;
+
+                  static const double ELEVATION_MAX_DEFAULT;
+                  static const double ELEVATION_MIN_DEFAULT;
+
+                  ElevationGrid();
+
+                  ElevationGrid(GridConfig config);
+
+                  ~ElevationGrid();
+
+                  Eigen::Vector3d getNormal( const Index& pos ) const;
+
+                  /** @brief get the normal vector at the given position
+                  */
+                  Eigen::Vector3d getNormal( const Eigen::Vector2d& pos ) const;
+
+                  /** @brief get the elevation at the given point 
+                  *
+                  * The underlying model assumes the height value to be at
+                  * the center of the cell, and a surface is approximated
+                  * using the getNormal. The Height value is the value of the
+                  * plane at that point.
+                  */
+                  double getMeanElevation( const Eigen::Vector2d& pos ) const;
+
+		};
+	}
+}
+
+
+#endif // __ENVIRE_MAPS_ELEVATION_GRID_HPP__
