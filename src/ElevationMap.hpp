d952e2fdc1a1d9cb01e62ef60ae435745a936720
Javier Hidalgo <javier.hidalgo_carrio@dfki.de>
2015-11-18 18:41:05 +0100

    src: ElevationMap is just a template version of GridMap

--- src/ElevationMap.hpp
+++ src/ElevationMap.hpp
@@ -3,45 +3,46 @@
 
 #include "GridMap.hpp"
 
-namespace envire 
+namespace envire {namespace maps
 {
-      namespace maps 
-      {
-            class ElevationMap : public GridMap
-            {
-            public:
 
-                  static const std::string ELEVATION;
-                  static const std::string ELEVATION_MIN;
-                  static const std::string ELEVATION_MAX;
+    typedef GridMap<double> ElevationMap;
 
-                  static const double ELEVATION_MAX_DEFAULT;
-                  static const double ELEVATION_MIN_DEFAULT;
 
-                  ElevationMap();
+    //class ElevationMap : public GridMap
+    //{
+    //public:
 
-                  ElevationMap(GridConfig config);
+    //      static const std::string ELEVATION;
+    //      static const std::string ELEVATION_MIN;
+    //      static const std::string ELEVATION_MAX;
 
-                  ~ElevationMap();
+    //      static const double ELEVATION_MAX_DEFAULT;
+    //      static const double ELEVATION_MIN_DEFAULT;
 
-                  Eigen::Vector3d getNormal( const Index& pos ) const;
+    //      ElevationMap();
 
-                  /** @brief get the normal vector at the given position
-                  */
-                  Eigen::Vector3d getNormal( const Eigen::Vector2d& pos ) const;
+    //      ElevationMap(GridConfig config);
 
-                  /** @brief get the elevation at the given point 
-                  *
-                  * The underlying model assumes the height value to be at
-                  * the center of the cell, and a surface is approximated
-                  * using the getNormal. The Height value is the value of the
-                  * plane at that point.
-                  */
-                  double getMeanElevation( const Eigen::Vector2d& pos ) const;
+    //      ~ElevationMap();
 
-            };
-      }
-}
+    //      Eigen::Vector3d getNormal( const Index& pos ) const;
+
+    //      /** @brief get the normal vector at the given position
+    //      */
+    //      Eigen::Vector3d getNormal( const Eigen::Vector2d& pos ) const;
+
+    //      /** @brief get the elevation at the given point 
+    //      *
+    //      * The underlying model assumes the height value to be at
+    //      * the center of the cell, and a surface is approximated
+    //      * using the getNormal. The Height value is the value of the
+    //      * plane at that point.
+    //      */
+    //      double getMeanElevation( const Eigen::Vector2d& pos ) const;
+
+    //};
+}}
 
 
 #endif // __ENVIRE_MAPS_ELEVATION_MAP_HPP__
