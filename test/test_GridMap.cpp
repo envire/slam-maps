d2e1afef318fada75a29140c3456cf057de9db8b
Javier Hidalgo <javier.hidalgo_carrio@dfki.de>
2015-11-18 18:41:24 +0100

    cmake: updarte cmake test: update test_suite

--- test/test_GridMap.cpp
+++ test/test_GridMap.cpp
@@ -1,129 +1,71 @@
-    #include <boost/test/unit_test.hpp>
+#include <boost/test/unit_test.hpp>
 #include <envire_maps/GridMap.hpp>
 
+#include <chrono>
+
 using namespace envire::maps;
-// the grid 20m x 50m
-size_t cellSizeX = 100;
-size_t cellSizeY = 100;
 
-// cell size 0.2m x 0.5m
-double scaleX = 0.2;
-double scaleY = 0.5;
-
-// the grid center is 20m x 50m
-double offsetX = 10;
-double offsetY = 25;
-
-GridConfig config(cellSizeX, cellSizeY, scaleX, scaleY, offsetX, offsetY);
-
-BOOST_AUTO_TEST_CASE(test_gridmap_has)
-{   
-    GridMap grid_map(config);
-
-    // check if the not existed grid exists: false
-    BOOST_CHECK_EQUAL(grid_map.hasGrid("double_grid"), false);
-
-    // check if the existed grid exists: true
-    grid_map.addGrid<double>("double_grid", std::numeric_limits<double>::infinity());
-    BOOST_CHECK_EQUAL(grid_map.hasGrid("double_grid"), true);
-
-    // check if the existence of the grid after it was removed
-    grid_map.removeGrid("double_grid"); 
-    BOOST_CHECK_EQUAL(grid_map.hasGrid("double_grid"), false);  
-}
-
-BOOST_AUTO_TEST_CASE(test_gridmap_add)
-{   
-    GridMap grid_map(config);
-
-    // add grid that does not exist until now: OK
-    BOOST_CHECK_NO_THROW(grid_map.addGrid<double>("double_grid", std::numeric_limits<double>::infinity())); 
-
-    // add grid with the key existed already and of the same type: THROW
-    BOOST_CHECK_THROW(grid_map.addGrid<double>("double_grid",  std::numeric_limits<double>::infinity()), std::exception);
-
-    // add grid with the key existed already but of the different type: THROW
-    BOOST_CHECK_THROW(grid_map.addGrid<int>("double_grid",  std::numeric_limits<int>::infinity()), std::exception);
-
-    // add grid with the key not existed and of the different type: true
-    BOOST_CHECK_NO_THROW(grid_map.addGrid<int>("int_grid",  std::numeric_limits<int>::infinity()));     
-}
-
-BOOST_AUTO_TEST_CASE(test_gridmap_config)
-{   
-    GridMap grid_map(config);
-
-    Grid<double> &grid = grid_map.addGrid<double>("double_grid", std::numeric_limits<double>::infinity());  
-
-    BOOST_CHECK_EQUAL(grid_map.getCellSizeX(), grid.getCellSizeX());
-    BOOST_CHECK_EQUAL(grid_map.getCellSizeY(), grid.getCellSizeY());
-    BOOST_CHECK_EQUAL(grid_map.getScaleX(), grid.getScaleX());  
-    BOOST_CHECK_EQUAL(grid_map.getScaleY(), grid.getScaleY());      
-    BOOST_CHECK_EQUAL(grid_map.getOffsetX(), grid.getOffsetX());    
-    BOOST_CHECK_EQUAL(grid_map.getOffsetY(), grid.getOffsetY());        
-}
-
-BOOST_AUTO_TEST_CASE(test_gridmap_get)
+BOOST_AUTO_TEST_CASE(test_grid_index)
 {
-    GridMap grid_map(config);
+    // check default constructor
+    GridBase::Index index;
+    BOOST_CHECK_EQUAL(index.x(), 0);
+    BOOST_CHECK_EQUAL(index.y(), 0);
 
-    Grid<double> &grid = grid_map.addGrid<double>("double_grid", std::numeric_limits<double>::infinity());      
+    // check constructor with parameter
+    GridBase::Index index2(2, 4);
+    BOOST_CHECK_EQUAL(index2.x(), 2);
+    BOOST_CHECK_EQUAL(index2.y(), 4);         
 
-    // get existing grid of correct type: OK
-    BOOST_CHECK_EQUAL(&(grid_map.getGrid<double>("double_grid")), &grid);
+    // check the "<" operator
+    BOOST_CHECK_EQUAL((index2 < GridBase::Index(index2.x + 1, index2.y + 1)), true);  
+    BOOST_CHECK_EQUAL((index2 < GridBase::Index(index2.x, index2.y)), false);       
+    BOOST_CHECK_EQUAL((index2 < GridBase::Index(index2.x, index2.y + 1)), true);    
+    BOOST_CHECK_EQUAL((index2 < GridBase::Index(index2.x, index2.y - 1)), false); 
+    BOOST_CHECK_EQUAL((index2 < GridBase::Index(index2.x - 1, index2.y + 1)), false);     
+    BOOST_CHECK_EQUAL((index2 < GridBase::Index(index2.x - 1, index2.y - 1)), false);     
 
-    // get existing grid of wrong type: THROW
-    BOOST_CHECK_THROW(grid_map.getGrid<int>("double_grid"), std::exception);
+    // check the "==" operator
+    BOOST_CHECK_EQUAL((index2 == GridBase::Index(index2.x, index2.y)), true);
+    BOOST_CHECK_EQUAL((index2 == GridBase::Index(index2.x, index2.y + 1)), false);        
+    BOOST_CHECK_EQUAL((index2 == GridBase::Index(index2.x + 1, index2.y)), false);    
+    BOOST_CHECK_EQUAL((index2 == GridBase::Index(index2.x + 1, index2.y + 1)), false);    
 
-    // get grid with wrong key: THROW
-    BOOST_CHECK_THROW(grid_map.getGrid<double>("no_grid"), std::exception); 
+    // check the "!=" operator
+    BOOST_CHECK_EQUAL((index2 != GridBase::Index(index2.x, index2.y)), false);
+    BOOST_CHECK_EQUAL((index2 != GridBase::Index(index2.x, index2.y + 1)), true);     
+    BOOST_CHECK_EQUAL((index2 != GridBase::Index(index2.x + 1, index2.y)), true); 
+    BOOST_CHECK_EQUAL((index2 != GridBase::Index(index2.x + 1, index2.y + 1)), true);     
 
-    // try to access deleted grid: THROW
-    grid_map.removeGrid("double_grid"); 
-    BOOST_CHECK_THROW(grid_map.getGrid<double>("double_grid"), std::exception); 
+    // check the sum operator
+    BOOST_CHECK((index2 + GridBase::Index(3, 5)) == GridBase::Index(5, 9));     
+
+    // TODO: check otherwise => it will not be 3,5
+    // check the diff operator
+    BOOST_CHECK((GridBase::Index(5, 9) - index2) == GridBase::Index(3, 5));     
 }
 
-BOOST_AUTO_TEST_CASE(test_gridmap_remove)
+// the grid 100 x 200 cells
+Eigen::Vector2ui num_cells(100, 200);
+
+// resolution 0.1m x 0.5m (therefore size will be 10 x 100m)
+Eigen::Vector2d resolution (0.1, 0.5);
+
+BOOST_AUTO_TEST_CASE(test_gridbase_default_constructor)
 {
-    GridMap grid_map(config);
+    GridBase grid_base;
+    BOOST_CHECK_EQUAL(grid_base.getNumCells(), Eigen::Vector2ui::Zero());
+    BOOST_CHECK_EQUAL(grid_base.getResolution(), Eigen::Vector2d::Zero());
+    BOOST_CHECK_EQUAL(grid_base.getSize(), Eigen::Vector2d::Zero());
 
-    grid_map.addGrid<double>("double_grid", std::numeric_limits<double>::infinity());       
+    BOOST_CHECK_EQUAL(grid_base.inGrid(GridBase::Index(0, 0)), false);
 
-    // remove grid not existed: false
-    BOOST_CHECK_EQUAL(grid_map.removeGrid("no_grid"), false);
+    Eigen::Vector2d pos;
+    BOOST_CHECK_EQUAL(grid_base.fromGrid(GridBase::Index(0, 0), pos), false);
 
-    // remove grid existed: true
-    BOOST_CHECK_EQUAL(grid_map.removeGrid("double_grid"), true);
+    GridBase::Index idx;
+    BOOST_CHECK_EQUAL(grid_base.toGrid(Eigen::Vector2d(0, 0), idx), false);
+
 }
 
-BOOST_AUTO_TEST_CASE(test_gridmap_removeall)
-{
-    GridMap grid_map(config);
 
-    grid_map.addGrid<double>("double_grid", std::numeric_limits<double>::infinity());       
-    grid_map.addGrid<int>("int_grid", std::numeric_limits<int>::infinity());        
-    grid_map.addGrid<char>("char_grid", std::numeric_limits<char>::infinity()); 
-
-    grid_map.removeAllGrids();
-
-    // check if the grids exist
-    BOOST_CHECK_EQUAL(grid_map.hasGrid("double_grid"), false);  
-    BOOST_CHECK_EQUAL(grid_map.hasGrid("int_grid"), false); 
-    BOOST_CHECK_EQUAL(grid_map.hasGrid("char_grid"), false);    
-}
-
-BOOST_AUTO_TEST_CASE(test_gridmap_keys)
-{
-    GridMap grid_map(config);
-
-    grid_map.addGrid<double>("double_grid", std::numeric_limits<double>::infinity());       
-    grid_map.addGrid<int>("int_grid", std::numeric_limits<int>::infinity());        
-    grid_map.addGrid<char>("char_grid", std::numeric_limits<char>::infinity()); 
-
-    std::vector<std::string> keys = grid_map.getAllGridKeys();
-
-    BOOST_CHECK_EQUAL(keys.size(), 3);
-    BOOST_CHECK_EQUAL(std::find(keys.begin(), keys.end(), "double_grid") !=  keys.end(), true);
-    BOOST_CHECK_EQUAL(std::find(keys.begin(), keys.end(), "int_grid") !=  keys.end(), true);
-    BOOST_CHECK_EQUAL(std::find(keys.begin(), keys.end(), "char_grid") !=  keys.end(), true);
-}
