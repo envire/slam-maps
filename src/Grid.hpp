7f2d7dd60aca79266389027dd01481fde96e57ae
Anna Born <anna.born@dfki.de>
2015-11-26 15:45:53 +0100

    add comparison operator to Index class

--- src/Grid.hpp
+++ src/Grid.hpp
@@ -11,21 +11,51 @@
 
 namespace envire { namespace maps 
 {
+
+    /**@brief type for the number of cells
+     */
+    typedef Eigen::Matrix<unsigned int, 2, 1> Vector2ui;  
+
+    typedef Eigen::Vector2d Vector2d;  
+
+    /**@brief Internal structure used to represent a position on the grid
+     * itself, as a cell index
+     */
+    class Index : public Eigen::Matrix<unsigned int, 2, 1>
+    {
+        public:
+            Index() 
+                : Eigen::Matrix<unsigned int, 2, 1>(0, 0)
+            {}
+
+            Index(unsigned int x, unsigned int y) 
+                : Eigen::Matrix<unsigned int, 2, 1>(x, y)
+            {}
+
+            bool operator<(const Index& other) const
+            {
+                return (x() < other.x() 
+                    || (x() == other.x() && y() < other.y()));
+            }           
+
+            bool operator>(const Index& other) const
+            {
+                return (x() > other.x() 
+                    || (x() == other.x() && y() > other.y()));
+            }         
+
+            // TODO: add exception if the other is bigger
+            // than this. due to uint
+            //Index operator-(const Index& other) const
+            //{
+            //}          
+    };
+
     /** Base class for all 2D gridbased maps.
      * - desribes the grid structure: resolution and number of cells in x- and y- axes.
      * - converts positon -> index and index -> position
      * - checks if the position or index is inside the grid
      */
-
-    /**@brief Internal structure used to represent a position on the grid
-     * itself, as a cell index
-     */
-    typedef Eigen::Vector2i Index;
-
-    /**@brief type for the number of cells
-     */
-    typedef Eigen::Matrix<unsigned int, 2, 1> Vector2ui;
-
     class Grid : public LocalMap
     {
         public:
@@ -33,13 +63,13 @@ namespace envire { namespace maps
 
         private:
             /** Resolution in X-axis and Y-axis **/
-            Eigen::Vector2d resolution;
+            Vector2d resolution;
 
             /** Number of cells in X-axis **/
             Vector2ui num_cells;
 
         public:            
-            Grid(const Eigen::Vector2d &resolution, const Vector2ui &num_cells);
+            Grid(const Vector2d &resolution, const Vector2ui &num_cells);
 
             ~Grid();
     };
--- test/test_Grid.cpp
+++ test/test_Grid.cpp
@@ -3,6 +3,55 @@
 
 using namespace envire::maps;
 
+BOOST_AUTO_TEST_CASE(test_grid_index)
+{
+    // check default constructor
+    Index index;
+    BOOST_CHECK_EQUAL(index.x(), 0);
+    BOOST_CHECK_EQUAL(index.y(), 0);
+
+    // check constructor with parameter
+    Index index2(2, 4);
+    BOOST_CHECK_EQUAL(index2.x(), 2);
+    BOOST_CHECK_EQUAL(index2.y(), 4);         
+
+    // check the "<" operator
+    BOOST_CHECK_EQUAL((index2 < Index(index2.x() + 1, index2.y() + 1)), true);  
+    BOOST_CHECK_EQUAL((index2 < Index(index2.x(), index2.y())), false);       
+    BOOST_CHECK_EQUAL((index2 < Index(index2.x(), index2.y() + 1)), true);    
+    BOOST_CHECK_EQUAL((index2 < Index(index2.x(), index2.y() - 1)), false); 
+    BOOST_CHECK_EQUAL((index2 < Index(index2.x() - 1, index2.y() + 1)), false);     
+    BOOST_CHECK_EQUAL((index2 < Index(index2.x() - 1, index2.y() - 1)), false);   
+
+    // check the "<" operator
+    BOOST_CHECK_EQUAL((index2 > Index(index2.x() - 1, index2.y() - 1)), true);  
+    BOOST_CHECK_EQUAL((index2 > Index(index2.x(), index2.y())), false);       
+    BOOST_CHECK_EQUAL((index2 > Index(index2.x(), index2.y() - 1)), true);    
+    BOOST_CHECK_EQUAL((index2 > Index(index2.x(), index2.y() + 1)), false); 
+    BOOST_CHECK_EQUAL((index2 > Index(index2.x() + 1, index2.y() - 1)), false);     
+    BOOST_CHECK_EQUAL((index2 > Index(index2.x() + 1, index2.y() + 1)), false);        
+
+    // check the "==" operator
+    BOOST_CHECK_EQUAL((index2 == Index(index2.x(), index2.y())), true);
+    BOOST_CHECK_EQUAL((index2 == Index(index2.x(), index2.y() + 1)), false);        
+    BOOST_CHECK_EQUAL((index2 == Index(index2.x() + 1, index2.y())), false);    
+    BOOST_CHECK_EQUAL((index2 == Index(index2.x() + 1, index2.y() + 1)), false);    
+
+    // check the "!=" operator
+    BOOST_CHECK_EQUAL((index2 != Index(index2.x(), index2.y())), false);
+    BOOST_CHECK_EQUAL((index2 != Index(index2.x(), index2.y() + 1)), true);     
+    BOOST_CHECK_EQUAL((index2 != Index(index2.x() + 1, index2.y())), true); 
+    BOOST_CHECK_EQUAL((index2 != Index(index2.x() + 1, index2.y() + 1)), true);     
+
+    // check the sum operator
+    BOOST_CHECK((index2 + Index(3, 5)) == Index(5, 9));     
+
+    BOOST_CHECK((Index(5, 9) - index2) == Index(3, 5));  
+
+    // Implement!
+    //BOOST_CHECK((index2 - Index(5, 9)) == Index(3, 5));  
+}
+
 BOOST_AUTO_TEST_CASE(test_grid_empty)
 {
     Grid grid(Eigen::Vector2d(0.1, 0.1), Vector2ui(100, 100));
