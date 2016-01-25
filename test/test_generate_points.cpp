c6118e8bce07dd501bb21c32f28b03e444a084ff
Christoph Hertzberg <chtz@informatik.uni-bremen.de>
2016-01-25 18:02:53 +0100

    Generate artificial point clouds as intersections of a LIDAR and a set of planes.

---
+++ test/test_generate_points.cpp
@@ -0,0 +1,41 @@
+
+#include <boost/test/unit_test.hpp>
+
+#include "tools/GeneratePointclouds.hpp"
+
+
+using namespace envire::maps;
+
+BOOST_AUTO_TEST_CASE(test_generate_PC)
+{
+    typedef Eigen::Hyperplane<double, 3> Plane;
+    LIDARSimulator lidar(Eigen::VectorXd::LinSpaced(20, -1.0, +1.0), Eigen::VectorXd::LinSpaced(30, -M_PI, +M_PI));
+//    LIDARSimulator lidar(Eigen::VectorXd::Constant(2, 0.0), Eigen::VectorXd::LinSpaced(30, -M_PI, +M_PI));
+    std::vector<Plane> scene;
+    for(int i=0; i<3; ++i)
+    {
+        scene.push_back(Plane(Eigen::Vector3d::Unit(i), 2.0));
+        scene.push_back(Plane(Eigen::Vector3d::Unit(i), -2.0));
+    }
+    Eigen::ArrayXXd ranges;
+    PointCloud pointcloud;
+    Eigen::Affine3d trafo;
+    trafo.setIdentity();
+    for(int k=0; k<1; ++k)
+    {
+//        trafo.translation().setRandom();
+
+        lidar.getRanges(ranges, scene, trafo, &pointcloud);
+
+        ranges.transposeInPlace();
+
+        for(PointCloud::const_iterator it = pointcloud.begin(); it != pointcloud.end(); ++it)
+        {
+//            std::cout << ranges(it - pointcloud.begin()) << ",\t" << it->getVector3fMap().transpose() << std::endl;
+            BOOST_CHECK_CLOSE((trafo * it->getVector3fMap().cast<double>()).lpNorm<Eigen::Infinity>(), 2.0, 0.01);
+        }
+
+    }
+}
+
+
---
+++ test/tools/GeneratePointclouds.hpp
@@ -0,0 +1,88 @@
+
+#ifndef TEST_TOOLS_GENERATEPOINTCLOUDS_HPP_
+#define TEST_TOOLS_GENERATEPOINTCLOUDS_HPP_
+
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+#include <cmath>
+
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
+namespace envire {
+namespace maps {
+
+typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
+
+/**
+ * LIDARSimulator simulates a 3d laser range finder, with laser beams at
+ */
+class LIDARSimulator
+{
+public:
+    /**
+     * Constructor taking vectors of all possible latitude and longitude angles.
+     * Both ranges must have at least one value. Latitude must be in [-PI/2, +PI/2], Longitude must be in [-PI, +PI].
+     * For a 2D Scanner, choose latitudeRange Eigen::Matrix<double, 1,1>(0.0),
+     * for a range of angles use the Eigen::VectorXd::LinSpaced function.
+     */
+    template<class Derived1, class Derived2>
+    LIDARSimulator(const Eigen::MatrixBase<Derived1>& latitudeRange_, const Eigen::MatrixBase<Derived2>& longitudeRange_) : latitudeRange(latitudeRange_), longitudeRange(longitudeRange_)
+    {
+        assert(latitudeRange.size()>=1    && latitudeRange.abs().maxCoeff() <= M_PI/2.0+0.001 );
+        assert(longitudeRange.size() >= 1 && longitudeRange.abs().maxCoeff() <= M_PI+0.001);
+    }
+
+    void getRanges(Eigen::Ref<Eigen::ArrayXXd> ranges, const std::vector<Eigen::Hyperplane<double, 3> >& scene, const Eigen::Affine3d & pose, PointCloud *pc = 0)
+    {
+        Eigen::DenseIndex height = latitudeRange.size();
+        Eigen::DenseIndex width = longitudeRange.size();
+        assert(ranges.rows()==height && ranges.cols() == width);
+        if(pc)
+        {
+            pc->height = height;
+            pc->width = width;
+            pc->resize(height * width);
+        }
+        for(Eigen::DenseIndex i=0; i<latitudeRange.size(); ++i)
+        {
+            double sinTheta = std::sin(latitudeRange(i)), cosTheta=std::cos(latitudeRange(i));
+            for(Eigen::DenseIndex j=0; j<longitudeRange.size(); ++j)
+            {
+                Eigen::Vector3d dir(cosTheta*std::cos(longitudeRange(j)), cosTheta*std::sin(longitudeRange(j)), sinTheta);
+                Eigen::ParametrizedLine<double, 3> line(pose.translation(), pose.linear()*dir);
+                double minDist = 1e99;
+//                std::cout << "Line = [" << line.origin().transpose() << "] + l*[" << line.direction().transpose() << "]: ";
+                for(size_t k=0; k<scene.size(); ++k)
+                {
+                    double dist = line.intersectionParameter(scene[k]);
+//                    std::cout << dist << " ";
+                    if(dist > 0.0 && dist < minDist)
+                        minDist = dist;
+                }
+//                std::cout << std::endl;
+                ranges(i, j) = minDist;
+                if(pc) // PCL first wants the column, then the row:
+                    pc->at(j, i).getVector3fMap() = line.pointAt(minDist).cast<float>();
+            }
+        }
+    }
+
+    void getRanges(Eigen::ArrayXXd& ranges, const std::vector<Eigen::Hyperplane<double, 3> >& scene, const Eigen::Affine3d & pose, PointCloud * pc = 0)
+    {
+        ranges.resize(latitudeRange.size(), longitudeRange.size());
+        getRanges(Eigen::Ref<Eigen::ArrayXXd>(ranges), scene, pose, pc);
+    }
+
+
+
+protected:
+    Eigen::ArrayXd latitudeRange, longitudeRange;
+};
+
+}  // namespace maps
+}  // namespace envire
+
+
+
+#endif /* TEST_TOOLS_GENERATEPOINTCLOUDS_HPP_ */
