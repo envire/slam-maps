bdfe036244acdfa139ef8449d356ca7e5afb437b
Janosch Machowinski <Janosch.Machowinski@dfki.de>
2016-02-19 17:55:29 +0100

    removed downcast related code

--- test/test_GridMap.cpp
+++ test/test_GridMap.cpp
@@ -7,33 +7,6 @@ using namespace envire::maps;
 
 double default_value = std::numeric_limits<double>::infinity();
 
-BOOST_AUTO_TEST_CASE(test_base_class)
-{
-
-    class FooBase
-    {
-        double barBase;
-    };
-    
-    class Foo : public FooBase
-    {
-        double bar;
-    };
-    
-    class barBase
-    {
-    };
-    
-    GridMap<Foo> *grid = new GridMap<Foo>();
-
-    GridMap<FooBase> *gridBase = grid->toBaseGrid<FooBase>();
-
-//     GridMap<barBase> *gridBase = grid->toBaseGrid<barBase>();
-
-    
-    delete grid;
-}
-
 BOOST_AUTO_TEST_CASE(test_grid_empty)
 {
     GridMap<double> *grid = new GridMap<double>();
