7c9d969da2e8448880dacf1241efa0a56db3d746
Christoph Hertzberg <chtz@informatik.uni-bremen.de>
2016-03-04 18:02:38 +0100

    re-enable MLS tests

--- test/viz/testMLS.cpp
+++ test/viz/testMLS.cpp
@@ -19,58 +19,58 @@ static void show_MLS(const MLSGrid& mls)
     }
 }
 
-// BOOST_AUTO_TEST_CASE( mlsviz_test ) 
-// {
-// //    GridConfig conf(300, 300, 0.05, 0.05, -7.5, -7.5);
-//     Vector2d res(0.05, 0.05);
-//     Vector2ui numCells(300, 300);
-// 
-//     MLSConfig mls_config;
-//     mls_config.updateModel = MLSConfig::SLOPE;
-//     //mls_config.updateModel = MLSConfig::KALMAN;
-//     MLSGrid *mls = new MLSGrid(numCells, res, mls_config);
-// 
-//     /** Translate the local frame (offset) **/
-//     mls->getLocalFrame().translation() << 0.5*mls->getSize(), 0;
-// 
-//     /** Equivalent to translate the grid in opposite direction **/
-//     //Eigen::Vector3d offset_grid;
-//     //offset_grid << -0.5*mls->getSize(), 0.00;
-//     //mls->translate(offset_grid);
-// 
-//     for (unsigned int x = 0; x < numCells.x(); ++x) for(float dx = -.5f; dx <0.49f; dx+=0.125)
-//     {
-//         float xx = x+dx-numCells.x()/2;
-//         float cs = std::cos(xx * M_PI/50);
-//         for (unsigned int y = 0; y < numCells.y(); ++y) for (float dy = -0.5f; dy<0.49; dy+=0.125)
-//         {
-//             float yy = y+dy-numCells.y()/2;
-//             float sn = std::sin(yy* M_PI/50);
-// 
-//             mls->mergePoint(Eigen::Vector3d(xx*res.x(), yy*res.y(), cs*sn));
-//             //mls->at(x, y).update(SurfacePatch(Eigen::Vector3f(dx*res.x(),dy*res.y(),cs*sn), 0.1));
-//             //mls->at(x, y).update(SurfacePatch(cs*sn+10, 0.1, 9, SurfacePatch::NEGATIVE));
-// //            mls->at(x, y).update(SurfacePatch(height, 0.1));
-//         }
-//     }
-// 
-//     show_MLS(*mls);
-// }
-/*
+BOOST_AUTO_TEST_CASE( mlsviz_test )
+{
+    //    GridConfig conf(300, 300, 0.05, 0.05, -7.5, -7.5);
+    Vector2d res(0.05, 0.05);
+    Vector2ui numCells(300, 300);
+
+    MLSConfig mls_config;
+    mls_config.updateModel = MLSConfig::SLOPE;
+    //mls_config.updateModel = MLSConfig::KALMAN;
+    MLSGrid *mls = new MLSGrid(numCells, res, mls_config);
+
+    /** Translate the local frame (offset) **/
+    mls->getGrid().getLocalFrame().translation() << 0.5*mls->getGrid().getSize(), 0;
+
+    /** Equivalent to translate the grid in opposite direction **/
+    //Eigen::Vector3d offset_grid;
+    //offset_grid << -0.5*mls->getSize(), 0.00;
+    //mls->translate(offset_grid);
+
+    for (unsigned int x = 0; x < numCells.x(); ++x) for(float dx = -.5f; dx <0.49f; dx+=0.125)
+    {
+        float xx = x+dx-numCells.x()/2;
+        float cs = std::cos(xx * M_PI/50);
+        for (unsigned int y = 0; y < numCells.y(); ++y) for (float dy = -0.5f; dy<0.49; dy+=0.125)
+        {
+            float yy = y+dy-numCells.y()/2;
+            float sn = std::sin(yy* M_PI/50);
+
+            mls->mergePoint(Eigen::Vector3d(xx*res.x(), yy*res.y(), cs*sn));
+            //mls->at(x, y).update(SurfacePatch(Eigen::Vector3f(dx*res.x(),dy*res.y(),cs*sn), 0.1));
+            //mls->at(x, y).update(SurfacePatch(cs*sn+10, 0.1, 9, SurfacePatch::NEGATIVE));
+            //            mls->at(x, y).update(SurfacePatch(height, 0.1));
+        }
+    }
+
+    show_MLS(*mls);
+}
+
 BOOST_AUTO_TEST_CASE(mls_loop)
 {
-//    GridConfig conf(150, 150, 0.1, 0.1, -7.5, -7.5);
+    //    GridConfig conf(150, 150, 0.1, 0.1, -7.5, -7.5);
     Eigen::Vector2d res(0.1, 0.1);
     Vector2ui numCells(150, 150);
 
     MLSConfig mls_config;
     mls_config.updateModel = MLSConfig::SLOPE;
-//    mls_config.updateModel = MLSConfig::KALMAN;
+    //    mls_config.updateModel = MLSConfig::KALMAN;
     mls_config.gapSize = 0.05f;
     mls_config.useNegativeInformation = false;
     float R = 5.0f, r=2.05f;
     MLSGrid *mls = new MLSGrid(numCells, res, mls_config);
-    mls->getLocalFrame().translation() << 0.5*mls->getSize(), 0;
+    mls->getGrid().getLocalFrame().translation() << 0.5*mls->getGrid().getSize(), 0;
 
     for (float alpha = 0; alpha < M_PI; alpha += M_PI/1024/4)
     {
@@ -89,5 +89,5 @@ BOOST_AUTO_TEST_CASE(mls_loop)
     }
 
     show_MLS(*mls);
-}*/
+}
 
--- test/viz/testMLS_LIDAR.cpp
+++ test/viz/testMLS_LIDAR.cpp
@@ -10,60 +10,60 @@
 using namespace envire::maps;
 
 
-// BOOST_AUTO_TEST_CASE(mls_simulate_LIDAR)
-// {
-// //    GridConfig conf(150, 150, 0.1, 0.1, -7.5, -7.5);
-//     Eigen::Vector2d res(0.125, 0.125);
-//     Vector2ui numCells(200, 200);
-// 
-//     MLSConfig mls_config;
-//     mls_config.updateModel = MLSConfig::SLOPE;
-// //    mls_config.updateModel = MLSConfig::KALMAN;
-//     mls_config.gapSize = 0.125f;
-//     mls_config.useNegativeInformation = true;
-//     MLSGrid *mls = new MLSGrid(numCells, res, mls_config);
-// 
-//     /** Translate the local frame (offset) **/
-//     mls->getLocalFrame().translation() << 0.5*mls->getSize(), 0;
-// 
-//     /** Equivalent to translate the grid in opposite direction **/
-//     //Eigen::Vector3d offset_grid;
-//     //offset_grid << -0.5*mls->getSize(), 0.00;
-//     //mls->translate(offset_grid);
-// 
-//     typedef Eigen::Hyperplane<double, 3> Plane;
-//     LIDARSimulator lidar(Eigen::VectorXd::LinSpaced(32, -16*M_PI/180, +16*M_PI/180), Eigen::VectorXd::LinSpaced(360, -M_PI, +M_PI));
-// //    LIDARSimulator lidar(Eigen::VectorXd::Constant(2, 0.0), Eigen::VectorXd::LinSpaced(30, -M_PI, +M_PI));
-//     std::vector<Plane> scene;
-//     {
-//         Eigen::Quaterniond q; q.coeffs().setRandom(); q.normalize();
-// 
-//         for(int i=0; i<3; ++i)
-//         {
-//             scene.push_back(Plane(q * Eigen::Vector3d::Unit(i), -2.0));
-//             scene.push_back(Plane(q * Eigen::Vector3d::Unit(i), 2.0));
-//         }
-//     }
-// 
-//     StandaloneVisualizer app;
-// 
-//    Eigen::ArrayXXd ranges;
-//    PointCloud pointcloud;
-//    Eigen::Affine3d trafo;
-//    trafo.setIdentity();
-//    int loop = 0;
-//    while (app.wait(1000))
-//    {
-//        if(++loop & 1023) continue;
-//        trafo.translation().setRandom();
-//        Eigen::Quaterniond q; q.coeffs().setRandom(); q.normalize();
-//        trafo.linear() = q.toRotationMatrix();
-// //        std::cout << trafo.matrix() << std::endl;
-// 
-//        lidar.getRanges(ranges, scene, trafo, &pointcloud);
-// 
-//        mls->mergePointCloud(pointcloud, trafo, false);
-//        app.updateData(*mls);
-//    }
-// 
-// }
+BOOST_AUTO_TEST_CASE(mls_simulate_LIDAR)
+{
+    //    GridConfig conf(150, 150, 0.1, 0.1, -7.5, -7.5);
+    Eigen::Vector2d res(0.125, 0.125);
+    Vector2ui numCells(200, 200);
+
+    MLSConfig mls_config;
+    mls_config.updateModel = MLSConfig::SLOPE;
+    //    mls_config.updateModel = MLSConfig::KALMAN;
+    mls_config.gapSize = 0.125f;
+    mls_config.useNegativeInformation = true;
+    MLSGrid *mls = new MLSGrid(numCells, res, mls_config);
+
+    /** Translate the local frame (offset) **/
+    mls->getGrid().getLocalFrame().translation() << 0.5*mls->getGrid().getSize(), 0;
+
+    /** Equivalent to translate the grid in opposite direction **/
+    //Eigen::Vector3d offset_grid;
+    //offset_grid << -0.5*mls->getSize(), 0.00;
+    //mls->translate(offset_grid);
+
+    typedef Eigen::Hyperplane<double, 3> Plane;
+    LIDARSimulator lidar(Eigen::VectorXd::LinSpaced(32, -16*M_PI/180, +16*M_PI/180), Eigen::VectorXd::LinSpaced(360, -M_PI, +M_PI));
+    //    LIDARSimulator lidar(Eigen::VectorXd::Constant(2, 0.0), Eigen::VectorXd::LinSpaced(30, -M_PI, +M_PI));
+    std::vector<Plane> scene;
+    {
+        Eigen::Quaterniond q; q.coeffs().setRandom(); q.normalize();
+
+        for(int i=0; i<3; ++i)
+        {
+            scene.push_back(Plane(q * Eigen::Vector3d::Unit(i), -2.0));
+            scene.push_back(Plane(q * Eigen::Vector3d::Unit(i), 2.0));
+        }
+    }
+
+    StandaloneVisualizer app;
+
+    Eigen::ArrayXXd ranges;
+    PointCloud pointcloud;
+    Eigen::Affine3d trafo;
+    trafo.setIdentity();
+    int loop = 0;
+    while (app.wait(1000))
+    {
+        if(++loop & 1023) continue;
+        trafo.translation().setRandom();
+        Eigen::Quaterniond q; q.coeffs().setRandom(); q.normalize();
+        trafo.linear() = q.toRotationMatrix();
+        //        std::cout << trafo.matrix() << std::endl;
+
+        lidar.getRanges(ranges, scene, trafo, &pointcloud);
+
+        mls->mergePointCloud(pointcloud, trafo, false);
+        app.updateData(*mls);
+    }
+
+}
