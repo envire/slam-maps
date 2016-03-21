69f12ec29d21cc3e8217488726b15043aa671e20
Javier Hidalgo <javier.hidalgo_carrio@dfki.de>
2016-03-21 13:15:13 +0100

    test: move eleviz_test down in the code in order to be able to use show method

--- test/viz/testElevation.cpp
+++ test/viz/testElevation.cpp
@@ -10,23 +10,6 @@
 #include <vizkit3d/GridVisualization.hpp>
 
 using namespace envire::maps;
-BOOST_AUTO_TEST_CASE(elevviz_test) 
-{
-    ElevationMap elev_map(Vector2ui(150, 250), Vector2d(0.1, 0.1));
-    elev_map.getLocalFrame().translation() << -0.5 * elev_map.getSize(), 0;
-
-    for (unsigned int x = 10; x < elev_map.getNumCells().x() - 10; ++x) 
-    {
-        float cs = std::cos(x * M_PI/50);
-        for (unsigned int y = 10; y < elev_map.getNumCells().y() - 10; ++y) 
-        {
-            float sn = std::sin(y * M_PI/50);
-
-            elev_map.at(x,y).elevation = cs * sn;
-            elev_map.at(x,y).elevation_min = -1 * cs * sn;
-        }
-    }
-}
 
 static void showElevationMap(const ElevationMap &elev_map)
 {
@@ -52,10 +35,36 @@ static void showElevationMap(const ElevationMap &elev_map)
     }
 }
 
-BOOST_AUTO_TEST_CASE(elevviz_loop)
+BOOST_AUTO_TEST_CASE(elevviz_test)
 {
     ElevationMap elev_map(Vector2ui(150, 250), Vector2d(0.1, 0.1));
     elev_map.getLocalFrame().translation() << 0.5 * elev_map.getSize(), 0;
+
+    for (unsigned int x = 10; x < elev_map.getNumCells().x() - 10; ++x) 
+    {
+        float cs = std::cos(x * M_PI/50);
+        for (unsigned int y = 10; y < elev_map.getNumCells().y() - 10; ++y) 
+        {
+            float sn = std::sin(y * M_PI/50);
+
+            elev_map.at(x,y).elevation = cs * sn;
+            elev_map.at(x,y).elevation_min = -1 * cs * sn;
+        }
+    }
+}
+
+BOOST_AUTO_TEST_CASE(elevviz_loop)
+{
+    ElevationMap elev_map(Vector2ui(150, 250), Vector2d(0.1, 0.1));
+
+    /** Translate the local frame (offset) **/
+    elev_map.getLocalFrame().translation() << 0.5 * elev_map.getSize(), 0;
+
+    /** Equivalent to translate the grid in opposite direction **/
+//    Eigen::Vector3d offset_grid;
+//    offset_grid << -0.5*elev_map.getSize(), 0.00;
+//    elev_map.translate(offset_grid);
+
     for(unsigned int x=0; x< elev_map.getNumCells().x(); ++x )
         for(unsigned int y=0; y<elev_map.getNumCells().y(); ++y)
         {
