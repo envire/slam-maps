127e20beb2b5e0beea83cfb93c7c14a26bc42d6b
Javier Hidalgo <javier.hidalgo_carrio@dfki.de>
2016-04-19 10:09:17 +0200

    src: add ContourMap class file

---
+++ src/geometric/ContourMap.hpp
@@ -0,0 +1,15 @@
+#ifndef __MAPS_CONTOURMAP_HPP_
+#define __MAPS_CONTOURMAP_HPP_
+
+#pragma once
+
+/** Maps classes **/
+#include <maps/geometric/LineSegment.hpp>
+#include <maps/geometric/GeometricMap.hpp>
+
+namespace maps
+{
+    typedef GeometricMap<LineSegment3d> ContourMap;
+}
+
+#endif /* __MAPS_CONTOURMAP_HPP_ */
