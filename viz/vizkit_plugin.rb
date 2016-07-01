a0cb9e223ea8617420b98858044d5c3080b36a28
Sascha Arnold <sascha.arnold@dfki.de>
2016-07-01 18:45:30 +0200

    viz: set correct type names in vizkit plugin registration

--- viz/vizkit_plugin.rb
+++ viz/vizkit_plugin.rb
@@ -2,8 +2,9 @@ Vizkit::UiLoader.register_3d_plugin('ElevationMapVisualization', 'maps', 'Elevat
 Vizkit::UiLoader.register_3d_plugin_for('ElevationMapVisualization', "/maps/ElevationMap", :updateData )
 
 Vizkit::UiLoader.register_3d_plugin('MLSMapVisualization', 'maps', 'MLSMapVisualization')
-Vizkit::UiLoader.register_3d_plugin_for('MLSMapVisualization', "/maps/MLSMap", :updateData )
+Vizkit::UiLoader.register_3d_plugin_for('MLSMapVisualization', "/maps/grid/MLSMap</maps/grid/MLSConfig/KALMAN>", :updateData )
+Vizkit::UiLoader.register_3d_plugin_for('MLSMapVisualization', "/maps/grid/MLSMap</maps/grid/MLSConfig/SLOPE>", :updateData )
 
 Vizkit::UiLoader.register_3d_plugin('ContourMapVisualization', "maps", 'ContourMapVisualization' )
-Vizkit::UiLoader.register_3d_plugin_for('ContourMapVisualization', "/maps/ContourMap", :updateData )
+Vizkit::UiLoader.register_3d_plugin_for('ContourMapVisualization', "/maps/geometric/GeometricMap</maps/geometric/LineSegment3d>", :updateData )
 
