d7c33325ca26b6b2407a68edcf3771c1ffbd05db
Sascha Arnold <sascha.arnold@dfki.de>
2016-08-08 16:42:07 +0200

    viz: alias typenames are now supported

--- viz/vizkit_plugin.rb
+++ viz/vizkit_plugin.rb
@@ -1,6 +1,6 @@
 Vizkit::UiLoader.register_3d_plugin('ElevationMapVisualization', 'maps', 'ElevationMapVisualization')
-Vizkit::UiLoader.register_3d_plugin_for('ElevationMapVisualization', "/maps/ElevationMap", :updateData )
-Vizkit::UiLoader.register_3d_plugin_for('ElevationMapVisualization', "/envire/core/SpatioTemporal</maps/ElevationMap>") do |plugin,sample,_|
+Vizkit::UiLoader.register_3d_plugin_for('ElevationMapVisualization', "/maps/grid/ElevationMap", :updateData )
+Vizkit::UiLoader.register_3d_plugin_for('ElevationMapVisualization', "/envire/core/SpatioTemporal</maps/grid/ElevationMap>") do |plugin,sample,_|
     if plugin.getVisualizationFrames.include? sample.frame_id
         plugin.setVisualizationFrame(sample.frame_id)
     end
@@ -8,29 +8,29 @@ Vizkit::UiLoader.register_3d_plugin_for('ElevationMapVisualization', "/envire/co
 end
 
 Vizkit::UiLoader.register_3d_plugin('GridMapVisualization', 'maps', 'GridMapVisualization')
-Vizkit::UiLoader.register_3d_plugin_for('GridMapVisualization', "/maps/GridMap<double>", :updateData )
-Vizkit::UiLoader.register_3d_plugin_for('GridMapVisualization', "/envire/core/SpatioTemporal</maps/GridMap<double>>") do |plugin,sample,_|
+Vizkit::UiLoader.register_3d_plugin_for('GridMapVisualization', "/maps/grid/GridMapD", :updateData )
+Vizkit::UiLoader.register_3d_plugin_for('GridMapVisualization', "/envire/core/SpatioTemporal</maps/grid/GridMapD>") do |plugin,sample,_|
     if plugin.getVisualizationFrames.include? sample.frame_id
         plugin.setVisualizationFrame(sample.frame_id)
     end
     plugin.updateData(sample.data)
 end
-Vizkit::UiLoader.register_3d_plugin_for('GridMapVisualization', "/maps/GridMap<float>", :updateData )
-Vizkit::UiLoader.register_3d_plugin_for('GridMapVisualization', "/envire/core/SpatioTemporal</maps/GridMap<float>>") do |plugin,sample,_|
+Vizkit::UiLoader.register_3d_plugin_for('GridMapVisualization', "/maps/grid/GridMapF", :updateData )
+Vizkit::UiLoader.register_3d_plugin_for('GridMapVisualization', "/envire/core/SpatioTemporal</maps/grid/GridMapF>") do |plugin,sample,_|
     if plugin.getVisualizationFrames.include? sample.frame_id
         plugin.setVisualizationFrame(sample.frame_id)
     end
     plugin.updateData(sample.data)
 end
-Vizkit::UiLoader.register_3d_plugin_for('GridMapVisualization', "/maps/GridMap<int>", :updateData )
-Vizkit::UiLoader.register_3d_plugin_for('GridMapVisualization', "/envire/core/SpatioTemporal</maps/GridMap<int>>") do |plugin,sample,_|
+Vizkit::UiLoader.register_3d_plugin_for('GridMapVisualization', "/maps/grid/GridMapI", :updateData )
+Vizkit::UiLoader.register_3d_plugin_for('GridMapVisualization', "/envire/core/SpatioTemporal</maps/grid/GridMapI>") do |plugin,sample,_|
     if plugin.getVisualizationFrames.include? sample.frame_id
         plugin.setVisualizationFrame(sample.frame_id)
     end
     plugin.updateData(sample.data)
 end
-Vizkit::UiLoader.register_3d_plugin_for('GridMapVisualization', "/maps/GridMap<char>", :updateData )
-Vizkit::UiLoader.register_3d_plugin_for('GridMapVisualization', "/envire/core/SpatioTemporal</maps/GridMap<char>>") do |plugin,sample,_|
+Vizkit::UiLoader.register_3d_plugin_for('GridMapVisualization', "/maps/grid/GridMapC", :updateData )
+Vizkit::UiLoader.register_3d_plugin_for('GridMapVisualization', "/envire/core/SpatioTemporal</maps/grid/GridMapC>") do |plugin,sample,_|
     if plugin.getVisualizationFrames.include? sample.frame_id
         plugin.setVisualizationFrame(sample.frame_id)
     end
@@ -38,15 +38,15 @@ Vizkit::UiLoader.register_3d_plugin_for('GridMapVisualization', "/envire/core/Sp
 end
 
 Vizkit::UiLoader.register_3d_plugin('MLSMapVisualization', 'maps', 'MLSMapVisualization')
-Vizkit::UiLoader.register_3d_plugin_for('MLSMapVisualization', "/maps/grid/MLSMap</maps/grid/MLSConfig/KALMAN>", :updateData )
-Vizkit::UiLoader.register_3d_plugin_for('MLSMapVisualization', "/envire/core/SpatioTemporal</maps/grid/MLSMap</maps/grid/MLSConfig/KALMAN>>") do |plugin,sample,_|
+Vizkit::UiLoader.register_3d_plugin_for('MLSMapVisualization', "/maps/grid/MLSMapKalman", :updateData )
+Vizkit::UiLoader.register_3d_plugin_for('MLSMapVisualization', "/envire/core/SpatioTemporal</maps/grid/MLSMapKalman>") do |plugin,sample,_|
     if plugin.getVisualizationFrames.include? sample.frame_id
         plugin.setVisualizationFrame(sample.frame_id)
     end
     plugin.updateData(sample.data)
 end
-Vizkit::UiLoader.register_3d_plugin_for('MLSMapVisualization', "/maps/grid/MLSMap</maps/grid/MLSConfig/SLOPE>", :updateData )
-Vizkit::UiLoader.register_3d_plugin_for('MLSMapVisualization', "/envire/core/SpatioTemporal</maps/grid/MLSMap</maps/grid/MLSConfig/SLOPE>>") do |plugin,sample,_|
+Vizkit::UiLoader.register_3d_plugin_for('MLSMapVisualization', "/maps/grid/MLSMapSloped", :updateData )
+Vizkit::UiLoader.register_3d_plugin_for('MLSMapVisualization', "/envire/core/SpatioTemporal</maps/grid/MLSMapSloped>") do |plugin,sample,_|
     if plugin.getVisualizationFrames.include? sample.frame_id
         plugin.setVisualizationFrame(sample.frame_id)
     end
@@ -54,8 +54,8 @@ Vizkit::UiLoader.register_3d_plugin_for('MLSMapVisualization', "/envire/core/Spa
 end
 
 Vizkit::UiLoader.register_3d_plugin('ContourMapVisualization', "maps", 'ContourMapVisualization' )
-Vizkit::UiLoader.register_3d_plugin_for('ContourMapVisualization', "/maps/geometric/GeometricMap</maps/geometric/LineSegment3d>", :updateData )
-Vizkit::UiLoader.register_3d_plugin_for('ContourMapVisualization', "/envire/core/SpatioTemporal</maps/geometric/GeometricMap</maps/geometric/LineSegment3d>>") do |plugin,sample,_|
+Vizkit::UiLoader.register_3d_plugin_for('ContourMapVisualization', "/maps/geometric/ContourMap", :updateData )
+Vizkit::UiLoader.register_3d_plugin_for('ContourMapVisualization', "/envire/core/SpatioTemporal</maps/geometric/ContourMap>") do |plugin,sample,_|
     if plugin.getVisualizationFrames.include? sample.frame_id
         plugin.setVisualizationFrame(sample.frame_id)
     end
