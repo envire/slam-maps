86a424eb4f5998b36c7d4f2908d9be9164e1aa8e
Sascha Arnold <sascha.arnold@dfki.de>
2016-10-17 19:54:16 +0200

    Added a voxel traversal helper class

---
+++ src/tools/VoxelTraversal.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <Eigen/Core>
+#include <maps/grid/Index.hpp>
+
+namespace maps { namespace tools
+{
+
+class VoxelTraversal
+{
+public:
+
+    struct RayElement
+    {
+        RayElement(const Eigen::Vector3i& idx, int z_step) : idx(idx.x(), idx.y()), z_first(idx.z()), z_last(idx.z()), z_step(z_step) {}
+
+        maps::grid::Index idx;
+        int32_t z_first;
+        int32_t z_last;
+        int32_t z_step;
+    };
+
+    /**
+     * Voxel traversal algorithm implemented according to:
+     * Amanatides, John, and Andrew Woo. "A fast voxel traversal algorithm for ray tracing." Eurographics. Vol. 87. No. 3. 1987.
+     */
+    static void computeRay(const Eigen::Vector3d& grid_res,
+                           const Eigen::Vector3d& origin, const Eigen::Vector3i& origin_idx, const Eigen::Vector3d& origin_cell_center,
+                           const Eigen::Vector3d& measurement, const Eigen::Vector3i& measurement_idx,
+                           std::vector<RayElement>& ray);
+};
+
+
+
+}}
