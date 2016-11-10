ea8bd51533c6edd2a094aeb30b972854610a1882
Arne Böckmann <arne.boeckmann@dfki.de>
2016-11-10 17:20:13 +0100

    use correct name for MLSMapPrecalculated viz

--- viz/MLSMapVisualization.hpp
+++ viz/MLSMapVisualization.hpp
@@ -39,7 +39,7 @@ namespace vizkit3d
             MLSMapVisualization();
             ~MLSMapVisualization();
 
-            Q_INVOKABLE void updateData(::maps::grid::MLSMap<::maps::grid::MLSConfig::PRECALCULATED> const &sample)
+            Q_INVOKABLE void updateData(maps::grid::MLSMapPrecalculated const &sample)
             {vizkit3d::Vizkit3DPlugin<::maps::grid::MLSMap<::maps::grid::MLSConfig::KALMAN>>::updateData(sample);}
             Q_INVOKABLE void updateData(maps::grid::MLSMapKalman const &sample)
             {vizkit3d::Vizkit3DPlugin<::maps::grid::MLSMap<::maps::grid::MLSConfig::KALMAN>>::updateData(sample);}
