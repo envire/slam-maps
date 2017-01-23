3c7c533bae1d9e428169003604c397accf4d3666
Sascha Arnold <sascha.arnold@dfki.de>
2017-01-23 20:22:26 +0100

    added a method which allows to project a given MLS into a TSDF map

--- src/grid/TSDFVolumetricMap.hpp
+++ src/grid/TSDFVolumetricMap.hpp
@@ -2,13 +2,14 @@
 
 #include "SurfacePatches.hpp"
 #include "VoxelGridMap.hpp"
+#include "MLSMap.hpp"
 
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <boost/serialization/access.hpp>
 #include <boost/serialization/nvp.hpp>
 #include <boost/serialization/export.hpp>
-
+#include <cmath>
 #include <base/TransformWithCovariance.hpp>
 
 namespace maps { namespace grid
@@ -35,25 +36,13 @@ public:
 
     template<int _MatrixOptions>
     void mergePointCloud(const std::vector< Eigen::Matrix<double, 3, 1, _MatrixOptions> >& pc, const base::TransformWithCovariance& pc2grid,
-                            const base::Vector3d& sensor_origin_in_pc = base::Vector3d::Zero(), double measurement_variance = 0.01)
-    {
-        Eigen::Vector3d sensor_origin_in_grid = pc2grid.getTransform() * sensor_origin_in_pc;
+                         const base::Vector3d& sensor_origin_in_pc = base::Vector3d::Zero(), double measurement_variance = 0.01);
 
-        for(typename std::vector< Eigen::Matrix<double, 3, 1, _MatrixOptions> >::const_iterator it = pc.begin(); it != pc.end(); ++it)
-        {
-            std::pair<Eigen::Vector3d, Eigen::Matrix3d> measurement_in_map = pc2grid.composePointWithCovariance(*it, Eigen::Matrix3d::Zero());
-            try
-            {
-                // TODO use variance in the direction of the measurement
-                mergePoint(sensor_origin_in_grid, measurement_in_map.first, measurement_variance + measurement_in_map.second(2,2));
-            }
-            catch(const std::runtime_error& e)
-            {
-                // TODO use glog or base log for all out prints of this library
-                std::cerr << e.what() << std::endl;
-            }
-        }
-    }
+    template<enum MLSConfig::update_model SurfaceType>
+    void projectMLSMap(const maps::grid::MLSMap<SurfaceType>& mls, const base::Transform3d& mls2grid,
+                       const Eigen::Vector2i& start_idx = Eigen::Vector2i(0,0),
+                       const Eigen::Vector2i& end_idx = Eigen::Vector2i(std::numeric_limits<int>::max(), std::numeric_limits<int>::max()),
+                       float z_min = -50.f, float z_max = 50.f, float truncation = 1.f);
 
     void mergePoint(const Eigen::Vector3d& sensor_origin, const Eigen::Vector3d& measurement, double measurement_variance = 0.01);
 
@@ -87,5 +76,66 @@ protected:
         ar & BOOST_SERIALIZATION_NVP(min_varaince);
     }
 };
+
+template<int _MatrixOptions>
+void TSDFVolumetricMap::mergePointCloud(const std::vector< Eigen::Matrix<double, 3, 1, _MatrixOptions> >& pc, const base::TransformWithCovariance& pc2grid,
+                                        const base::Vector3d& sensor_origin_in_pc, double measurement_variance)
+{
+    Eigen::Vector3d sensor_origin_in_grid = pc2grid.getTransform() * sensor_origin_in_pc;
+
+    for(typename std::vector< Eigen::Matrix<double, 3, 1, _MatrixOptions> >::const_iterator it = pc.begin(); it != pc.end(); ++it)
+    {
+        std::pair<Eigen::Vector3d, Eigen::Matrix3d> measurement_in_map = pc2grid.composePointWithCovariance(*it, Eigen::Matrix3d::Zero());
+        try
+        {
+            // TODO use variance in the direction of the measurement
+            mergePoint(sensor_origin_in_grid, measurement_in_map.first, measurement_variance + measurement_in_map.second(2,2));
+        }
+        catch(const std::runtime_error& e)
+        {
+            // TODO use glog or base log for all out prints of this library
+            std::cerr << e.what() << std::endl;
+        }
+    }
+}
+
+template<enum MLSConfig::update_model SurfaceType>
+void TSDFVolumetricMap::projectMLSMap(const maps::grid::MLSMap<SurfaceType>& mls, const base::Transform3d& mls2grid,
+                                      const Eigen::Vector2i& start_idx, const Eigen::Vector2i& end_idx,
+                                      float z_min, float z_max, float truncation)
+{
+    base::Transform3d grid2mls = mls2grid.inverse();
+    Eigen::Vector3d res = getVoxelResolution();
+    Eigen::Vector3i max_idx;
+    max_idx << end_idx.array().min(getNumCells().array().cast<int>()), (int)std::floor(z_max / res.z());
+    Eigen::Vector3i min_idx;
+    min_idx << start_idx, (int)std::floor(z_min / res.z());
+    Eigen::Vector3i idx;
+    Eigen::Vector3d cell_center;
+    Eigen::Vector3d closest_point;
+    for(idx.y() = min_idx.y(); idx.y() < max_idx.y(); idx.y() = idx.y() + 1)
+    {
+        for(idx.x() = min_idx.x(); idx.x() < max_idx.x(); idx.x() = idx.x() + 1)
+        {
+            for(idx.z() = min_idx.z(); idx.z() < max_idx.z(); idx.z() = idx.z() + 1)
+            {
+                if(fromVoxelGrid(idx, cell_center))
+                {
+                    cell_center = grid2mls * cell_center;
+                    if(mls.getClosestContactPoint(cell_center, closest_point))
+                    {
+                        Eigen::Vector3d diff = (cell_center - closest_point);
+                        float distance = diff.norm();
+                        if(distance < truncation)
+                        {
+                            VoxelCellType& cell = getVoxelCell(idx);
+                            cell.update(std::copysign(distance, diff.z()), 0.01, truncation, min_varaince);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
 
 }}
